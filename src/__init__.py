7059e19dceb66d5c30f8b08bfdb255b72e6e6332
albertolanzini <alberto.lanzini.maria@gmail.com>
2024-05-25 18:54:33 +0200

    feat: added .env to correctly handle pytest

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_root not in sys.path:
+    sys.path.append(project_root)
