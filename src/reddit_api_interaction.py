4b8ae4b9a0b7a7b1de7c7874a4804aee566ea687
albertolanzini <alberto.lanzini.maria@gmail.com>
2024-05-25 18:55:58 +0200

    chore: added check for directory existance

--- src/reddit_api_interaction.py
+++ src/reddit_api_interaction.py
@@ -2,17 +2,12 @@ import praw
 import os
 import pandas as pd
 from typing import List, Dict
-
-
 from src.utils import get_reddit_api_keys, get_config_path
 
 
 def fetch_reddit_data(subreddits, num_posts):
     config_path = get_config_path()
     api_keys = get_reddit_api_keys(config_path)
-
-    # print(f"{api_keys} are:")
-    # print(f"client_id: {api_keys['client_id']}, client_secret: {api_keys['client_secret']}, user_agent: {api_keys['user_agent']}")
 
     reddit = praw.Reddit(client_id=api_keys['client_id'],
                          client_secret=api_keys['client_secret'],
@@ -34,21 +29,25 @@ def fetch_reddit_data(subreddits, num_posts):
         return posts
 
     config_dir = os.path.dirname(config_path)
-
     all_data = []
+
     for subreddit in subreddits:
         subreddit_data = get_subreddit_data(subreddit)
         df = pd.DataFrame(subreddit_data)
-        csv_filename = os.path.join(config_dir, f'reddit_data/{subreddit}_data.csv')
+        csv_dir = os.path.join(config_dir, 'reddit_data')
+        os.makedirs(csv_dir, exist_ok=True)  # ensure the directory exists
+        csv_filename = os.path.join(csv_dir, f'{subreddit}_data.csv')
+        # print(f"Saving CSV to: {csv_filename}")
         df.to_csv(csv_filename, index=False)
         all_data.extend(subreddit_data)
-    
+
     return all_data
+
 
 if __name__ == '__main__':
     # Define subreddits and number of posts
-    subreddits = ['nursing']
-    num_posts = 10
+    subreddits = ['nursing', 'Teachers']
+    num_posts = 1000
     
     # Fetch Reddit data
     all_data = fetch_reddit_data(subreddits, num_posts)
