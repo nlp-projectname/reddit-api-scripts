cf618edd2d77cbc1a9abd3169c292a741541a8fa
albertolanzini <alberto.lanzini.maria@gmail.com>
2024-05-26 02:17:14 +0200

    chore: fixed conversion dict + sum processing

--- src/text_preprocessing.py
+++ src/text_preprocessing.py
@@ -25,32 +25,38 @@ def expand_contractions(text: str) -> str:
     return contractions_re.sub(replace, text)
 
 
+# TODO: check this is the desired behavior, otherwise simply eliminate
 def convert_emojis(text: str) -> str:
     for emot in UNICODE_EMOJI:
         text = text.replace(
             emot,
             " ".join(UNICODE_EMOJI[emot].replace(",", "").replace(":", "").split()),
         )
-    return text
+    return text.replace("_", " ")
 
 
 def preprocess_text(text: str) -> str:
+    # print("Original:", text)
     text = text.lower()
-    text = re.sub(r"http\S+|www\S+|@\S+", "", text)  # remove URLs or usernames
-
-    text = re.sub(r"[^A-Za-z\s]", "", text)  # remove chars, punctuation and numbers
+    # print("Lowercased:", text)
     text = expand_contractions(text)
+    # print("Contractions expanded:", text)
     text = convert_emojis(text)
-
+    # print("Emojis converted:", text)
+    text = re.sub(r"http\S+|www\S+|@\S+", "", text)  # remove URLs or usernames
+    # print("URLs removed:", text)
+    text = re.sub(r"[^A-Za-z\s]", "", text)  # remove chars, punctuation and numbers
+    # print("Chars removed:", text)
     tokens = word_tokenize(text)
-
+    # print("Tokenized:", tokens)
     tokens = [word for word in tokens if word not in stop_words]
-
+    # print("Stopwords removed:", tokens)
     tokens = [PorterStemmer().stem(word) for word in tokens]
+    # print("Stemmed:", tokens)
     tokens = [WordNetLemmatizer().lemmatize(word) for word in tokens]
-
-    # join back into a string
+    # print("Lemmatized:", tokens)
     text = " ".join(tokens)
+    # print("Final text:", text)
     return text
 
 
