da7fd121d8df9310b05d464707fe06a64cb5d184
albertolanzini <alberto.lanzini.maria@gmail.com>
2024-05-28 12:13:20 +0200

    feat: added FeatureExtractor class

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,4 @@ import sys
 
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
-
-project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if project_root not in sys.path:
-    sys.path.append(project_root)
